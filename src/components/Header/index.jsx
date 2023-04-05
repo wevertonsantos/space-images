c432d96631ba0ede6f66558672d1d1a9ccbd4454
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-05 15:13:59 -0300

    style on input and container, add too font-family

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,8 @@ export default function Header (){
     return(
         <header className={styles.header}>
             <h1>Space Images</h1>
-            <div>
-                <input type="text" placeholder="O que você procura?"/>
+            <div className={styles.header__container}>
+                <input className={styles.header__input} type="text" placeholder="O que você procura?"/>
                 <img src={search}/>
             </div>
         </header>
