215801e5eb3cf5c9b4f22ab46b4c2549cb724882
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-05 17:01:46 -0300

    menu navegator

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
 
 export default function Home(){
     return(
-        <Header/>
+        <>
+            <Header />
+            <Menu />
+        </>
     )
 }
