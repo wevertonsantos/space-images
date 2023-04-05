f76d1b29cc430f23d4e480f0f2bdc1973a7a884b
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-05 17:15:42 -0300

    create banner component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,21 @@
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
+import styles from './Home.module.scss'
+import banner from './banner.png'
 
 export default function Home(){
     return(
         <>
             <Header />
-            <Menu />
+            <main className={styles.main}>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
